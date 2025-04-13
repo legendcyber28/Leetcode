f6fb24a62dd42531816bcb6c622733f1120762ab
Prashant Katheriya <108624644+legendcyber28@users.noreply.github.com>
2025-04-13 20:41:31 +0530

    Time: 0 ms (100%), Space: 9.6 MB (74.3%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(begin(s),end(s));
-        sort(begin(t), end(t));
-        
-        return s==t;
+        vector<int> count(26,0);
+
+        for(char &ch : s)
+        {
+            count[ch-'a']++;
+        }
+        for(char &ch : t)
+        {
+            count[ch-'a']--;
+        }
+        for(int c:count)
+        {
+            if(c!=0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 };
