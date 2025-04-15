2b00c52a97fc82b7f1454aaeef00e7b76a86d573
Prashant Katheriya <108624644+legendcyber28@users.noreply.github.com>
2025-04-15 21:09:05 +0530

    Time: 134 ms (76.57%), Space: 145.6 MB (75.52%) - LeetHub

--- 2117-find-original-array-from-doubled-array/2117-find-original-array-from-doubled-array.cpp
+++ 2117-find-original-array-from-doubled-array/2117-find-original-array-from-doubled-array.cpp
@@ -2,38 +2,32 @@ class Solution {
 public:
     vector<int> findOriginalArray(vector<int>& changed) {
         int n = changed.size();
-        
-        //odd length
-        if(n%2 != 0) {
-            return {};
+        vector<int> original;
+        if(n%2!=0)
+        {
+           return {};
         }
-        
-        sort(begin(changed), end(changed));
-        
-        map<int, int> mp;
-        
-        //store frequency of each number
-        for(int &num : changed) {
-            mp[num]++;
+        sort(begin(changed),end(changed));
+        unordered_map<int,int> mp;
+        for(auto it : changed)
+        {
+            mp[it]++;
         }
-        
-        vector<int> result;
-        
-        for(int &num : changed) {
-            int twice = 2*num;
-            
-            if(mp[num] == 0) continue; //skip
-            
-            if(mp.find(twice) == mp.end() || mp[twice] == 0) {
+        for(auto it:changed)
+        {
+            int twice = 2*it;
+            if(mp[it]==0)
+            {
+                continue; //skip
+            }
+            if(mp.find(twice)==mp.end() || mp[twice]==0)
+            {
                 return {};
             }
-            
-            result.push_back(num);
-            
-            mp[num]--;
+            original.push_back(it);
+            mp[it]--;
             mp[twice]--;
         }
-        
-        return result;
+        return original;
     }
 };
