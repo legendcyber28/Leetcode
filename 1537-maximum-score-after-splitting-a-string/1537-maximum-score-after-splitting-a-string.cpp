c5493e8c5ebca9f69bad5d354be35e9f89fe01b0
Prashant Katheriya <108624644+legendcyber28@users.noreply.github.com>
2025-04-19 20:32:41 +0530

    Time: 0 ms (100%), Space: 8.8 MB (30.28%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -2,31 +2,26 @@ class Solution {
 public:
     int maxScore(string s) {
         int n = s.size();
+        int total_ones = count(begin(s),end(s), '1');
 
         int result = INT_MIN;
-        for(int i = 0; i<n-1; i++)
-        {
-            // for zero count
-            int zero_count = 0;
-            for(int j = 0; j<=i;j++)
-            {
-                if(s[j]=='0')
-                {
-                    zero_count++;
-                }
-            }
-            
-            //for one count
-            int one_count = 0;
-            for(int j = i+1; j<n;j++)
-            {
-                if(s[j]=='1')
-                {
-                    one_count++;
-                }
-            }
+        int zeros = 0;
+        int ones = 0;
+        
+        // right one = total_ones-ones;
 
-            result  =  max(result, zero_count+ one_count);
+        for(int i = 0; i<=n-2; i++)
+        {
+            if(s[i]=='1')
+            {
+                ones++;
+            }
+            else{
+                zeros++;
+            }
+            int right_ones = total_ones - ones;
+
+            result = max(result, zeros + right_ones);
         }
         return result;
     }
