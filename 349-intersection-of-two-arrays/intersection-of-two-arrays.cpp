0554db683c9542ba61e3c8ae0f9565b86f7b3e9c
Prashant Katheriya <108624644+legendcyber28@users.noreply.github.com>
2025-05-04 08:32:19 -0700

    Time: 3 ms (37.98%) | Memory: 14.2 MB (66.82%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -3,15 +3,16 @@ public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         // set me unique value hi store hote hai yad rakhiye ise
         unordered_set<int> set1 (nums1.begin(), nums1.end());
-        unordered_set<int> result;
+        vector<int>result; //without using 2nd set
 
         for(int &num : nums2)
         {
             if(set1.find(num) != set1.end())
             {
-                result.insert(num);
+                result.push_back(num);
+                set1.erase(num);
             }
         }
-        return vector<int>(result.begin(), result.end());
+        return result;
     }
 };
