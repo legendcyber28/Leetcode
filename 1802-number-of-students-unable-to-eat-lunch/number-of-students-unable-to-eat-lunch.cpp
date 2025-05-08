78e926f72878d0339409f8c28a1df98cc8b5b61c
Prashant Katheriya <108624644+legendcyber28@users.noreply.github.com>
2025-05-08 20:52:40 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (69.14%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        queue<int> que;
-        stack<int> st;
-        for(int &nums : students)
-        {
-            que.push(nums);
-        }
-        int m = sandwiches.size();
-        for(int i = m-1; i>=0 ;i--)
-        {
-               st.push(sandwiches[i]);
-        }
-        int unable_eat = 0;
-        while(!que.empty() && unable_eat < que.size())
-        {
-            if(que.front() == st.top())
-            {
-                que.pop();
-                st.pop();
-                unable_eat = 0;
+        int n = students.size();
 
-            }
-            else
-            {
-                que.push(que.front());
-                que.pop();
-                unable_eat++;
-            }
-        }
+        int arr[2]; 
+        //arr[0] student which like the 1 or square sandwiches
+        // arr[1] student which like the 0 or circle sandwiches
 
-        return que.size();
-        
+        for(int &nums: students)
+        {
+            arr[nums]++;
+            
+        }
+        for(int i = 0; i<n ; i++)
+        {
+            int sand = sandwiches[i];
+            if(arr[sand] == 0 )
+            {
+                return n-i;
+            }
+            arr[sand]--;
+        }
+        return 0;
     }
 };
